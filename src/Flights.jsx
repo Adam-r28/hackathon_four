287670e2d33b4ae3e5115c1aa29b073bca578956
matimi1 <milos.matiaska@gmail.com>
2022-07-01 10:38:37 +0200

    first

--- src/Flights.jsx
+++ src/Flights.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 export default function Flights() {
 
-    const url = `https://api.skypicker.com/flights?fly_from=PRG&fly_to=VLC&limit=5&date_from=${dateFrom}&partner=data4youcbp202106`;
+    const url = `https://api.skypicker.com/flights?fly_from=PRG&fly_to=VLC&limit=5&date_from=1/8/2022&partner=data4youcbp202106`;
 
 
     const [flights, setFlights] = useState([]);
@@ -11,9 +11,9 @@ export default function Flights() {
     const fetchData = async () => {
         const resp = await fetch(url);
         const data = await resp.json();
-        setFlights(data);
+        setFlights(data.data);
         setDataLoaded(true);
-        console.log(data)
+        console.log(data.data)
     };
 
     useEffect(() => {
